5e0a3a7406be142f38ab325079196e1d96d30507
Mitchellnkem <Mitchellnkemgmailcomgit config --global user.email Mitchellnkemgmail.com>
2025-01-12 22:43:29 +0000

    sunday commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<meta content="" name="description">
+	<meta content="" name="keywords">
+	<title>G I T B | GRADING</title>
+
+	<!-- Favicons -->
+	 <link rel="icon" href="assets/img/favicon.png">
+	 <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
+
+	 <!-- Google Fonts -->
+	 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i">
+
+	 <!-- Vendor CSS Files -->
+	 <link rel="shortcut icon" href="assets/img/ronk1.jpg">
+	 <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+
+	<!-- <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet"> -->
+	  	<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+		<link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+		<link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+		<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+	<!-- Template Main CSS File -->
+		<link href="assets/css/style22.css" rel="stylesheet">
+
+
+</head>
+<body>
+	<!-- ------------------ HEADER -------------------- -->
+	 <header id="header" class="fixed-top">
+		<div class="container d-flex align-items-center">
+			<a class="navbar-brand" href="index.php"><img src="assets/img/student-grade.png" alt="logo" style="width:30p;height:30px;"></a>
+			<h1 class="logo mr-auto"><a href="index.php">GITB | GRADING </a></h1>
+
+			<nav class="nav-menu d-none d-lg-block">
+				
+			</nav>
+		</div>
+	 </header>
+
+</body>
+</html>
