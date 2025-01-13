5efb3b06339606c75ff82f14ae6cca0d608ee328
Mitchellnkem <Mitchellnkemgmailcomgit config --global user.email Mitchellnkemgmail.com>
2025-01-13 15:56:25 +0000

    done with index page

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 
 
 </head>
-<body>
+<main>
 	<!-- ------------------ HEADER -------------------- -->
 	 <header id="header" class="fixed-top">
 		<div class="container d-flex align-items-center">
@@ -38,10 +38,509 @@
 			<h1 class="logo mr-auto"><a href="index.php">GITB | GRADING </a></h1>
 
 			<nav class="nav-menu d-none d-lg-block">
-				
+				<ul>
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Courses</a></li>
+					<li><a href="#">Contact</a></li>
+					
+					<li class="drop-down"><a href="">Login <i class="bx bxs-arrow-down"></i></a>
+						<ul>
+							<li><a href="adminLogin.php">Administrator</a></li>
+							<li><a href="studentLogin.php">Student</a></li>
+						</ul>
+					</li>
+				</ul>
 			</nav>
+			<!-- nav menu ends -->
 		</div>
 	 </header>
+
+	<!-- ------------------ HEADER ENDS -------------------- -->
+
+	<!-- ------------------ MAIN CONTENT / HERO SECTION -------------------- -->
+
+	<section id="hero" class="d-flex justify-content-center align-items-center" style="background-image: url('assets/img/hero-bg.png');">
+		<div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+			<h1>GENIUS IT <br> BRAINERY</h1>
+			<h2>UNLEASH BRILLIANCE - IGNITE IMPACT</h2>
+			<a href="#" class="btn-get-started">Read more</a>
+		</div>
+			
+	</section> 
+
+	<!-- ------------------ MAIN CONTENT / HERO SECTION ENDS -------------------- -->
+
+
+	<!-- MAIN 2 CONTENT -->
+
+	<main id="main">
+	<!-- ABOUT SECTION BEGINS -->
+
+	<section id="about" class="about">
+		<div class="container" data-aos="fade-up">
+			
+		
+			<div class="section-title">
+				<h2>Our Mission</h2>
+				<p> To empower students to excel academically and socially, by providing them with the tools and resources they need to succeed. </p>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+					<img src="assets/img/ronk1.jpg" class="img-fluid" alt="">
+				</div>
+
+				<div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nobis exercitationem eaque eveniet dolor dolores porro, dolorem repellat, beatae veritatis asperiores illum laborum laboriosam iste cupiditate sint temporibus officia praesentium.</p>
+
+					<a href="#" class="learn-more-btn">Read More</a>
+				</div>
+			</div>
+
+		</div>
+	</section>
+
+	<!-- ABOUT SECTION ENDS -->
+
+
+	<!-- COUNT SECTION BEGINS -->
+		<section id="counts" class="counts section-bg">
+			<div class="container">
+			
+			<div class="row counters">
+
+				<div class="col-lg-3 col-6 text-center">
+					<span data-toggle="counter-up">1069</span>
+					<p>Students</p>
+				</div>
+
+				<div class="col-lg-3 col-6 text-center">
+					<span data-toggle="counter-up">9</span>
+					<p>Department</p>
+				</div>
+
+				<div class="col-lg-3 col-6 text-center">
+					<span data-toggle="counter-up">200</span>
+					<p>Events</p>
+				</div>
+
+				<div class="col-lg-3 col-6 text-center">
+					<span data-toggle="counter-up">15</span>
+					<p>Faculties</p>
+				</div>
+
+
+			</div>
+
+			</div>
+		</section>
+
+	<!-- COUNT SECTION ENDS -->
+
+	  <!-- ======= Why Us Section ======= -->
+    <!-- <section id="why-us" class="why-us">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-4 d-flex align-items-stretch">
+            <div class="content">
+              <h3>Why Choose Mentor?</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.
+              </p>
+              <div class="text-center">
+                <a href="about.html" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-8 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-boxes d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-receipt"></i>
+                    <h4>Corporis voluptates sit</h4>
+                    <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+                  </div>
+                </div>
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-cube-alt"></i>
+                    <h4>Ullamco laboris ladore pan</h4>
+                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                  </div>
+                </div>
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-images"></i>
+                    <h4>Labore consequatur</h4>
+                    <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+                  </div>
+                </div>
+              </div>
+            </div>End .content -->
+			</div>
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
+			<!-- ======= Features Section ======= -->
+			<!-- <section id="features" class="features">
+			<div class="container" data-aos="fade-up">
+
+				<div class="row" data-aos="zoom-in" data-aos-delay="100">
+				<div class="col-lg-3 col-md-4">
+					<div class="icon-box">
+					<i class="ri-store-line" style="color: #ffbb2c;"></i>
+					<h3><a href="">Lorem Ipsum</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+					<div class="icon-box">
+					<i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+					<h3><a href="">Dolor Sitema</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+					<div class="icon-box">
+					<i class="ri-calendar-todo-line" style="color: #e80368;"></i>
+					<h3><a href="">Sed perspiciatis</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
+					<div class="icon-box">
+					<i class="ri-paint-brush-line" style="color: #e361ff;"></i>
+					<h3><a href="">Magni Dolores</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4">
+					<div class="icon-box">
+					<i class="ri-database-2-line" style="color: #47aeff;"></i>
+					<h3><a href="">Nemo Enim</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4">
+					<div class="icon-box">
+					<i class="ri-gradienter-line" style="color: #ffa76e;"></i>
+					<h3><a href="">Eiusmod Tempor</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4">
+					<div class="icon-box">
+					<i class="ri-file-list-3-line" style="color: #11dbcf;"></i>
+					<h3><a href="">Midela Teren</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4">
+					<div class="icon-box">
+					<i class="ri-price-tag-2-line" style="color: #4233ff;"></i>
+					<h3><a href="">Pira Neve</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4">
+					<div class="icon-box">
+					<i class="ri-anchor-line" style="color: #b2904f;"></i>
+					<h3><a href="">Dirada Pack</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4">
+					<div class="icon-box">
+					<i class="ri-disc-line" style="color: #b20969;"></i>
+					<h3><a href="">Moton Ideal</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4">
+					<div class="icon-box">
+					<i class="ri-base-station-line" style="color: #ff5828;"></i>
+					<h3><a href="">Verdo Park</a></h3>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 mt-4">
+					<div class="icon-box">
+					<i class="ri-fingerprint-line" style="color: #29cc61;"></i>
+					<h3><a href="">Flavor Nivelanda</a></h3>
+					</div>
+				</div>
+				</div>
+
+			</div>
+			</section>End Features Section -->
+
+			<!-- ======= Popular Courses Section ======= -->
+			<!-- <section id="popular-courses" class="courses">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+				<h2>Courses</h2>
+				<p>Popular Courses</p>
+				</div>
+
+				<div class="row" data-aos="zoom-in" data-aos-delay="100">
+
+				<div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+					<div class="course-item">
+					<img src="assets/img/course-1.jpg" class="img-fluid" alt="...">
+					<div class="course-content">
+						<div class="d-flex justify-content-between align-items-center mb-3">
+						<h4>Web Development</h4>
+						<p class="price">$169</p>
+						</div>
+
+						<h3><a href="course-details.html">Website Design</a></h3>
+						<p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+						<div class="trainer d-flex justify-content-between align-items-center">
+						<div class="trainer-profile d-flex align-items-center">
+							<img src="assets/img/trainers/trainer-1.jpg" class="img-fluid" alt="">
+							<span>Antonio</span>
+						</div>
+						<div class="trainer-rank d-flex align-items-center">
+							<i class="bx bx-user"></i>&nbsp;50
+							&nbsp;&nbsp;
+							<i class="bx bx-heart"></i>&nbsp;65
+						</div>
+						</div>
+					</div>
+					</div>
+				</div> End Course Item -->
+
+				<!-- <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+					<div class="course-item">
+					<img src="assets/img/course-2.jpg" class="img-fluid" alt="...">
+					<div class="course-content">
+						<div class="d-flex justify-content-between align-items-center mb-3">
+						<h4>Marketing</h4>
+						<p class="price">$250</p>
+						</div>
+
+						<h3><a href="course-details.html">Search Engine Optimization</a></h3>
+						<p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+						<div class="trainer d-flex justify-content-between align-items-center">
+						<div class="trainer-profile d-flex align-items-center">
+							<img src="assets/img/trainers/trainer-2.jpg" class="img-fluid" alt="">
+							<span>Lana</span>
+						</div>
+						<div class="trainer-rank d-flex align-items-center">
+							<i class="bx bx-user"></i>&nbsp;35
+							&nbsp;&nbsp;
+							<i class="bx bx-heart"></i>&nbsp;42
+						</div>
+						</div>
+					</div>
+					</div>
+				</div> End Course Item -->
+
+				<!-- <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+					<div class="course-item">
+					<img src="assets/img/course-3.jpg" class="img-fluid" alt="...">
+					<div class="course-content">
+						<div class="d-flex justify-content-between align-items-center mb-3">
+						<h4>Content</h4>
+						<p class="price">$180</p>
+						</div>
+
+						<h3><a href="course-details.html">Copywriting</a></h3>
+						<p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+						<div class="trainer d-flex justify-content-between align-items-center">
+						<div class="trainer-profile d-flex align-items-center">
+							<img src="assets/img/trainers/trainer-3.jpg" class="img-fluid" alt="">
+							<span>Brandon</span>
+						</div>
+						<div class="trainer-rank d-flex align-items-center">
+							<i class="bx bx-user"></i>&nbsp;20
+							&nbsp;&nbsp;
+							<i class="bx bx-heart"></i>&nbsp;85
+						</div>
+						</div>
+					</div>
+					</div>
+				</div> End Course Item -->
+
+				<!-- </div>
+
+			</div>
+			</section>End Popular Courses Section -->
+
+			<!-- ======= Trainers Section ======= -->
+			<!-- <section id="trainers" class="trainers">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+				<h2>Trainers</h2>
+				<p>Our Professional Trainers</p>
+				</div>
+
+				<div class="row" data-aos="zoom-in" data-aos-delay="100">
+				<div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+					<div class="member">
+					<img src="assets/img/trainers/trainer-1.jpg" class="img-fluid" alt="">
+					<div class="member-content">
+						<h4>Walter White</h4>
+						<span>Web Development</span>
+						<p>
+						Magni qui quod omnis unde et eos fuga et exercitationem. Odio veritatis perspiciatis quaerat qui aut aut aut
+						</p>
+						<div class="social">
+						<a href=""><i class="icofont-twitter"></i></a>
+						<a href=""><i class="icofont-facebook"></i></a>
+						<a href=""><i class="icofont-instagram"></i></a>
+						<a href=""><i class="icofont-linkedin"></i></a>
+						</div>
+					</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+					<div class="member">
+					<img src="assets/img/trainers/trainer-2.jpg" class="img-fluid" alt="">
+					<div class="member-content">
+						<h4>Sarah Jhinson</h4>
+						<span>Marketing</span>
+						<p>
+						Repellat fugiat adipisci nemo illum nesciunt voluptas repellendus. In architecto rerum rerum temporibus
+						</p>
+						<div class="social">
+						<a href=""><i class="icofont-twitter"></i></a>
+						<a href=""><i class="icofont-facebook"></i></a>
+						<a href=""><i class="icofont-instagram"></i></a>
+						<a href=""><i class="icofont-linkedin"></i></a>
+						</div>
+					</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+					<div class="member">
+					<img src="assets/img/trainers/trainer-3.jpg" class="img-fluid" alt="">
+					<div class="member-content">
+						<h4>William Anderson</h4>
+						<span>Content</span>
+						<p>
+						Voluptas necessitatibus occaecati quia. Earum totam consequuntur qui porro et laborum toro des clara
+						</p>
+						<div class="social">
+						<a href=""><i class="icofont-twitter"></i></a>
+						<a href=""><i class="icofont-facebook"></i></a>
+						<a href=""><i class="icofont-instagram"></i></a>
+						<a href=""><i class="icofont-linkedin"></i></a>
+						</div>
+					</div>
+					</div>
+				</div>
+
+				</div>
+
+			</div>
+			</section>End Trainers Section -->
+
+
+	</main>
+	<!-- MAIN 2 CONTENT ENDS -->
+
+
+
+
+
+
+
+
+	<!-- FOOTER BEGINS HERE -->
+
+	<footer id="footer">
+		<div class="footer-top">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-lg-3 col-md-6 footer-contact">
+						<h3>GENIUS IT BRAINERY</h3>
+						<p>
+							P.O. BOX 1413, <br>
+							Rams Horn Building, Opposite Winners Chapel, Barracks Road<br><br>
+							<strong>Phone:</strong> 0536331169 | 0204295241<br>
+							<strong>E-mail:</strong> info@geniusitbrainery.com<br>
+						</p>
+					</div>
+						<div class="col-lg-2 col-md-6 footer-links">
+							<h4>Useful Links</h4>
+							<ul>
+								<li><i class="bx > "></i><a href="#">Home</a></li>
+								<li><i class="bx > "></i><a href="#">About us</a></li>
+								<li><i class="bx > "></i><a href="#">Services</a></li>
+								<li><i class="bx > "></i><a href="#">Terms of service</a></li>
+								<li><i class="bx > "></i><a href="#">Privacy policy</a></li>
+							</ul>
+						</div>
+
+						<div class="col-lg-2 col-md-6 footer-links">
+							<h4>Our Services</h4>
+							<ul>
+								<li><i class="bx > "></i><a href="#">Service 1</a></li>
+								<li><i class="bx > "></i><a href="#">Service 2</a></li>
+								<li><i class="bx > "></i><a href="#">Service 3</a></li>
+								<li><i class="bx > "></i><a href="#">Service 4</a></li>
+								<li><i class="bx > "></i><a href="#">Service 5</a></li>
+							</ul>
+						</div>
+
+						<div class="col-lg-4 col-md-6 footer-newsletter">
+							<h4>Join Our Newsletter</h4>
+							<p>Some random text spaces here. For now, just a demo text all over!</p>
+							<form action="" method="post">
+							<input type="email" name="email"><input type="submit" value="Subscribe Now!!">
+							</form>
+						</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container d-md-flex py-4">
+
+			<div class="mr-md-auto text-center text-md-left">
+				<div class="copyright">
+					&copy; <strong><span>GITB</span></strong> - <strong> <?php echo date('Y');?> - Developed By Ghost </strong>
+				</div>
+				<div class="credits">
+					<!-- All the links in the footer should remain intact. -->
+					<!-- You can delete the links only if you purchased the pro version. -->
+					<!-- Licensing information: https://bootstrapmade.com/license/ -->
+					<!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/ -->
+					<!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+				</div>
+			</div>
+			<div class="social-links text-center text-md-right pt-3 pt-md-0">
+				<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+				<a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+				<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+				<a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+				<a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+			</div>
+		</div>
+
+
+	</footer>
+
+	<!-- END OF FOOTER -->
+
+	<a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></a>
+	<div class="preloader"></div>
+
+
+	<!-- vendor Javascript Files -->
+	<script src="assets/vendor/jquery/jquery.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+	<script src="assets/vendor/php-email-form/validate.js"></script>
+	<script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+	<script src="assets/vendor/counterup/counterup.min.js"></script>
+	<script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+	<script src="assets/vendor/aos/aos.js"></script>
+
+
+
+	<!-- Template Main Javascript File -->
+	 <script src="assets/js/main.js"></script>
 
 </body>
 </html>
